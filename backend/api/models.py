19f54c7124f5baab1ca0060b882819e4f7e1c3a0
Dan Johnson <ComputerDruid@gmail.com>
2013-01-11 15:56:37 -0800

    backend: define string representation of models

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,15 +3,21 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
 	username = models.CharField(max_length=100, unique=True, primary_key=True)
+	def __unicode__(self):
+		return self.username
 
 class Venue(models.Model):
 	id = models.IntegerField(unique=True, primary_key=True)
 	name = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.name
 
 class MenuItem(models.Model):
 	id = models.IntegerField(unique=True, primary_key=True)
 	venue = models.ForeignKey(Venue)
 	name = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.name
 
 class Preference(models.Model):
 	user = models.ForeignKey(User)
