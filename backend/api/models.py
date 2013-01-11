ed0476386343be0f65725a587c27f641f249785a
Dan Johnson <ComputerDruid@gmail.com>
2013-01-11 15:44:04 -0800

    backend: define model for api app

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=100, unique=True, primary_key=True)
+
+class Venue(models.Model):
+	id = models.IntegerField(unique=True, primary_key=True)
+	name = models.CharField(max_length=255)
+
+class MenuItem(models.Model):
+	id = models.IntegerField(unique=True, primary_key=True)
+	venue = models.ForeignKey(Venue)
+	name = models.CharField(max_length=255)
+
+class Preference(models.Model):
+	user = models.ForeignKey(User)
+	item = models.ForeignKey(MenuItem)
+	LIKE = 1
+	DISLIKE = 2
+	VALUE_CHOICES = (
+		(LIKE, 'Would eat again'),
+		(DISLIKE, 'Would not eat again')
+	)
+	value = models.IntegerField(choices=VALUE_CHOICES)
