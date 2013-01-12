b15ca533e9ae7248948bc1af1aac0c896359784b
Usman Masood <usmanm@locu.com>
2013-01-12 20:05:37 +0000

    Fixing importers

--- backend/api/rating_importer.py
+++ backend/api/rating_importer.py
@@ -1,26 +1,37 @@
+from collections import defaultdict
+
 from models import MenuItem, User, Rating
 
 import csv
 
+def valid_item(votes, row):
+  return len([vote for vote in votes[row['id']] if vote == 'Yes']) >= 2 and not 'international orange' in row['venue-name'].lower()
+
 def import_ratings(f):
-	with open(f) as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			if row["this_dish_is_an_entree_on_a_dinner_menu__as_opposed_to_beverages_appetizers_or_sides"] == "Yes":
-				try:
-					item = MenuItem.objects.get(locu_id=row["id"])
-				except MenuItem.DoesNotExist:
+  votes = defaultdict(list)
+  with open(f) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      votes[row['id']].append(row["this_dish_is_an_entree_on_a_dinner_menu__as_opposed_to_beverages_appetizers_or_sides"])
+
+  with open(f) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      if valid_item(votes, row):
+        try:
+          item = MenuItem.objects.get(locu_id=row["id"])
+        except MenuItem.DoesNotExist:
 					print "Could not find a menuitem", row["id"]
 					continue
-				username = row["_worker_id"]
-				try:
-					user = User.objects.get(username=username)
-				except User.DoesNotExist:
+        username = row["_worker_id"]
+        try:
+          user = User.objects.get(username=username)
+        except User.DoesNotExist:
 					user = User(username=username)
 					user.save()
-				if row["i_would_like_to_eat_this_dish"] == "Yes":
+        if row["i_would_like_to_eat_this_dish"] == "Yes":
 					value = Rating.LIKE
-				else:
+        else:
 					value = Rating.DISLIKE
-				r = Rating(user=user, menu_item=item, value=value)
-				r.save()
+        r = Rating(user=user, menu_item=item, value=value)
+      r.save()
--- backend/import_run.py
+++ backend/import_run.py
@@ -1,9 +1,7 @@
-def main():
+def run():
 	from api.importer import import_items
 	import_items("../turk/data/menu_item.json")
 	from api.rating_importer import import_ratings
 	import_ratings("../turk/data/training.csv")
 	from ml import ML
 	ML.build()
-
-main()
