cbf3cb02b54b83a37167ab5a11fe4c67e2dc932a
Dan Johnson <ComputerDruid@gmail.com>
2013-01-12 10:35:12 -0800

    add hacky importer run script
    This can (sometimes) be run as python manage.py shell <
    importer-run.py

---
+++ backend/importer-run.py
@@ -0,0 +1,9 @@
+try:
+	from api.importer import import_items
+	import_items("../turk/data/menu_item.json")
+	from api.rating_importer import import_ratings
+	import_ratings("../turk/data/training.csv")
+	from ml import ML
+	ML.build()
+except Exception as e:
+	raise e
