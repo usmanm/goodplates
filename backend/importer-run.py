7937c29b64760b1333541e73eecc4cb9db4fa40d
Dan Johnson <ComputerDruid@gmail.com>
2013-01-12 11:41:02 -0800

    make importer script more verbose

--- backend/importer-run.py
+++ backend/importer-run.py
@@ -1,9 +1,12 @@
 def main():
 	from api.importer import import_items
+	print("importing menu items")
 	import_items("../turk/data/menu_item.json")
+	print("importing ratings")
 	from api.rating_importer import import_ratings
 	import_ratings("../turk/data/training.csv")
 	from ml import ML
+	print("rebuilding model")
 	ML.build()
 
 main()
